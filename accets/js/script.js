324c36aca34f993c520ef70f3026ed6c9959d1c7
TalisMan701 <boss.kompaneec@mail.ru>
2019-07-06 22:04:26 +0500

    Сделал блять обновление карзины

--- accets/js/script.js
+++ accets/js/script.js
@@ -4,3 +4,13 @@ if (many_tovar > 0) {
 } else {
   $("#many_tovar").addClass("many_tovar_null");
 }
+jQuery("#many_tovar").bind('DOMSubtreeModified', function () { // отслеживаем изменение содержимого блока 2
+  var many_tovar = Number($("#many_tovar").html());
+  if (many_tovar > 0) {
+    jQuery("#many_tovar").addClass("many_tovar");
+    jQuery("#many_tovar").removeClass("many_tovar_null");
+  } else {
+    jQuery("#many_tovar").addClass("many_tovar_null");
+    jQuery("#many_tovar").removeClass("many_tovar");
+  }
+});
